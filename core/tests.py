be1c5213abaea716ab1bd1646d61b9a58ab10e24
Levi Macario <levimacario@gmail.com>
2019-09-04 21:59:44 -0300

    adicionado testes unitários a view ResultView

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class ResultViewTestCase(TestCase):
+
+    def setUp(self):
+        self.url = '/result/{0}/{1}/'
+        self.client = Client()
+
+    def test_view_ok(self):
+        response = self.client.get(self.url.format('2020', 'bissexto'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/result.html')
+
+    def test_view_404(self):
+        response = self.client.get(self.url.format('2020', 'naobissexto'))
+        self.assertEquals(response.status_code, 404)
+
+    def test_leap_context(self):
+        response = self.client.get(self.url.format('2020', 'bissexto'))
+        self.assertTrue('status' in response.context)
+        self.assertEquals(response.context['status'], 'success')
+        self.assertTrue('msg' in response.context)
+        self.assertEquals(response.context['msg'], 'O ano 2020 é bissexto.')
+
+    def test_no_leap_context(self):
+        response = self.client.get(self.url.format('2019', 'nao-bissexto'))
+        self.assertTrue('status' in response.context)
+        self.assertEquals(response.context['status'], 'danger')
+        self.assertTrue('msg' in response.context)
+        self.assertEquals(response.context['msg'], 'O ano 2019 não é bissexto.')
